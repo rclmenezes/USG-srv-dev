d1f4a880ebc60835d9880cf480290fc5acc236db
Josh Chen <joshchen@princeton.edu>
2012-02-25 19:54:38 +0000

    Revert "Revert "gitignore for apache2.conf; added script to copy local_settings""
    This reverts commit 3ca49d338e675e22731c8c1621d5a55cb3c1e340.

---
+++ scripts/copy_local_settings.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Enter your username:"
+read username
+home="/home/$username/USG-srv-dev"
+echo "This may overwrite any existing local_settings.py files in all subdirectories of $home. Continue? [y/n]"
+read to_continue
+if [[ $to_continue != 'y' ]]; then
+    exit
+fi
+
+for folder in `ls -l $home | grep ^d | awk '{ print $NF; }'`; do
+    for file in `find /srv/$folder | grep local_settings.py`; do
+        echo "cp $file $home/${file:5}"
+        cp $file $home/${file:5}
+    done
+done
