a4b277f3850a99138ff5705499825063349b7f75
Josh Chen <joshchen@princeton.edu>
2012-03-11 05:23:58 +0000

    small fixes to files in base dir

--- scripts/copy_local_settings.sh
+++ scripts/copy_local_settings.sh
@@ -6,6 +6,7 @@ home="/home/$username/USG-srv-dev"
 echo "This may overwrite any existing local_settings.py files in all subdirectories of $home. Continue? [y/n]"
 read to_continue
 if [[ $to_continue != 'y' ]]; then
+    echo "Did not enter 'y': will not copy"
     exit
 fi
 
