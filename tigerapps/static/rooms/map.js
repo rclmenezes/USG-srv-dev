e48613debd6fd3bc9d47d3bc579a244b4c4cb6f4
John O'Neill <jconeill@dev.tigerapps.org>
2012-04-05 09:10:58 +0000

    basic fomatting, map expansion/contraction animation

--- tigerapps/static/rooms/map.js
+++ tigerapps/static/rooms/map.js
@@ -14,9 +14,22 @@ function initialize() {
     google.maps.event.addListener(marker, 'click', function() {
         alert('You clicked on Scully!');
     });
-    
+
     google.maps.event.addListener(map, 'bounds_changed', function() {
         if (!map.getBounds().contains(marker.getPosition()))
             alert('went out of bounds!');
     });
+
+	
+	/* map exapansion/contraction animation */
+	var mapc = document.getElementById("map_canvas");
+	var datap = document.getElementById("dataPanel");
+	document.getElementById("expandMap").addEventListener("click", function()
+		{mapc.style.height = "80%"; datap.style.height="20%"})
+	document.getElementById("contractMap").addEventListener("click", function()
+		{mapc.style.height = "0%"; datap.style.height="100%"})
+	document.getElementById("restoreMap").addEventListener("click", function()
+		{mapc.style.height = "50%"; datap.style.height="50%"})
+
+
 }
