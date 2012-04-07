83147026ce479e19c72033fcb547ec272cc582f0
Kevin Mantel <kevinmantel2@gmail.com>
2012-04-07 02:34:45 +0000

    Map's bounds now change when the pane resizes, the 'clicked' and 'went out of bounds' popups are now console logs

--- tigerapps/static/rooms/map.js
+++ tigerapps/static/rooms/map.js
@@ -1,23 +1,26 @@
 function initialize() {
-    var myOptions = {
+    var myOptions = 
+    {
         center: new google.maps.LatLng(40.346446, -74.657013),
         zoom: 16,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map = new google.maps.Map(document.getElementById("map_canvas"),
-                                  myOptions);
-    var marker = new google.maps.Marker( {
+    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+    var marker = new google.maps.Marker( 
+    {
         position: new google.maps.LatLng(40.344267, -74.654422),
         map: map,
     });
     
-    google.maps.event.addListener(marker, 'click', function() {
-        alert('You clicked on Scully!');
+    google.maps.event.addListener(marker, 'click', function() 
+    {
+        console.log('You clicked on Scully!');
     });
 
-    google.maps.event.addListener(map, 'bounds_changed', function() {
+    google.maps.event.addListener(map, 'bounds_changed', function() 
+    {
         if (!map.getBounds().contains(marker.getPosition()))
-            alert('went out of bounds!');
+            console.log('went out of bounds!');
     });
 
 	
@@ -25,11 +28,23 @@ function initialize() {
 	var mapc = document.getElementById("map_canvas");
 	var datap = document.getElementById("dataPanel");
 	document.getElementById("expandMap").addEventListener("click", function()
-		{mapc.style.height = "80%"; datap.style.height="20%"})
+		{
+		    mapc.style.height = "80%"; 
+		    datap.style.height="20%";
+		    setTimeout(function(){google.maps.event.trigger(map, 'resize');}, 300);
+	    })
 	document.getElementById("contractMap").addEventListener("click", function()
-		{mapc.style.height = "0%"; datap.style.height="100%"})
+		{
+		    mapc.style.height = "0%"; 
+		    datap.style.height="100%";
+		    setTimeout(function(){google.maps.event.trigger(map, 'resize');}, 300);
+	    })
 	document.getElementById("restoreMap").addEventListener("click", function()
-		{mapc.style.height = "50%"; datap.style.height="50%"})
+		{
+		    mapc.style.height = "50%"; 
+		    datap.style.height="50%";
+		    setTimeout(function(){google.maps.event.trigger(map, 'resize');}, 300);
+	    })
 
 
 }
