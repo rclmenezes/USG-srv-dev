14d766e6f082d5f82979d358816306a6ebc9af27
Josh Chen <joshchen@princeton.edu>
2012-04-11 01:59:23 +0000

    added storage.views

--- tigerapps/storage/views.py
+++ tigerapps/storage/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.core.mail import send_mail
+from storage.models import *
+from storage.forms import *
+from django_cas.decorators import login_required, user_passes_test
+
+def paypal(request):
+    return HttpResponse('0')
