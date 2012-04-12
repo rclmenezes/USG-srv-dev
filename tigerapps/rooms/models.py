fefc9a04df84a7a175d8eafdda44edd7420d2865
John O'Neill <jconeill@dev.tigerapps.org>
2012-04-12 00:23:43 +0000

    Rooms: updated models with rooms structure

--- tigerapps/rooms/models.py
+++ tigerapps/rooms/models.py
@@ -1,20 +1,55 @@
 from django.db import models
 
-# Create your models here.
 class Draw(models.Model):
     name = models.CharField(max_length=24)
 
     def __unicode__(self):
         return self.name
-    
+
+# campus buildings    
 class Building(models.Model):
     name = models.CharField(max_length=30)
     pdfname = models.CharField(max_length=30)
     availname = models.CharField(max_length=30)
-    draw = models.ManyToManyField(Draw)
+    draw = models.ManyToManyField('Draw')
     lat = models.FloatField()
     lon = models.FloatField()
 
     def __unicode__(self):
         return self.name
 
+# door rooms
+class Rooms(models.Model):
+	
+	GENDER_CHOICES = (
+		('E', 'Either'),
+		('M', 'Male'),
+		('F', 'Female'),
+		('X', 'Mixed')
+	)
+
+	BATHROOM_CHOICES = (
+		('PU', 'Public'),
+		('PR', 'Private'),
+		('SH', 'Shared')
+	)
+
+	# room numbers can include letters
+	number = models.CharField(max_length=10)
+	sqft = models.IntegerField()
+	occ = models.IntegerField()
+	building = models.ForeignKey('Building')
+	subfree = models.BooleanField()
+	numrooms = models.IntegerField()
+	floor = models.IntegerField()
+	gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+	avail = models.BooleanField()
+	adjacent = models.CharField(max_length=10)
+	# ADA accessible
+	ada = model.BooleanField()
+	# bi-level room
+	bi = model.BooleanField();
+	# connecting single
+	con = model.BooleanField();
+	bathroom = models.CharField(max_length=2, choiced=BATHROOM_CHOICES)
+	
