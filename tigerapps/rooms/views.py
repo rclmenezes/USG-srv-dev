b9daa0eef1fd77a4c8ab937a18dad1e4e3ff1174
Lily Healey <lhealey@princeton.edu>
2012-04-13 17:40:28 +0000

    worked on adding rooms list to templates

--- tigerapps/rooms/views.py
+++ tigerapps/rooms/views.py
@@ -3,9 +3,15 @@
 from django.shortcuts import render_to_response
 #from rooms.models import Poll
 from django.contrib.auth.decorators import login_required, user_passes_test
+from models import Draw, Building, Room
 
 @login_required
 def index(request):
-    return render_to_response('rooms/base.html')
-
-
+    draw_list = Draw.objects.order_by('name')
+    building_list = Building.objects.order_by('name')
+    #buildingbydraw_dict = {}
+    #for draw in draw_list:
+        #buildingbydraw_dict[draw] = Building.objects.filter(draw__name=draw)
+    #    buildingbydraw_dict[draw] = Building.objects
+    room_list = Room.objects.order_by('number')
+    return render_to_response('rooms/base_dataPanel.html', locals())
