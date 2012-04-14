d076d9598a3def12b9aa4707974c04625502e072
Lily Healey <lhealey@princeton.edu>
2012-04-14 12:31:49 +0000

    rooms: made draw tabs separate pages

--- tigerapps/rooms/urls.py
+++ tigerapps/rooms/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     url(r'^$', 'rooms.views.index'),
     (r'^login/?$', 'django_cas.views.login'),
     (r'^logout/?$', 'django_cas.views.logout'),
-    
+    (r'^drawid/(?P<drawid>\d{1})$', 'rooms.views.draw'),
     # Admin interface
     (r'^admin/', include(admin.site.urls)),
 )
