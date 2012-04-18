0dc2c0232995ce0bd2f8bc3c992a706ec17c57c1
Adam Suczewski <asuczewski@gmail.com>
2012-04-18 03:23:21 +0000

    paypal

--- tigerapps/storage/views.py
+++ tigerapps/storage/views.py
@@ -1,30 +1,3 @@
-"""
-from django.shortcuts import render_to_response, get_object_or_404
-from django.http import HttpResponse, Http404
-from django.core.mail import send_mail
-from storage.models import *
-from storage.forms import *
-from django_cas.decorators import login_required, user_passes_test
-from paypal.standard.forms import PayPalPaymentsForm
-from django.conf import settings
-from django.core.urlresolvers import reverse
-
-def paypal(request):
-    paypal_dict = {
-        "business": settings.PAYPAL_RECEIVER_EMAIL,
-        "amount": "1.00",
-        "invoice": "unique-invoice-id",
-        "notify_url": "%s%s" % (settings.SITE_NAME, reverse('paypal-ipn')),
-        "return_url": "http://www.example.com/your-return-location/",
-        "cancel_return": "http://www.example.com/your-cancel-location/",
-    }
-
-    # Create the instance.
-    form = PayPalPaymentsForm(initial=paypal_dict)
-    context = {"form": form.sandbox()}
-    return render_to_response("storage/paypal.html", context)
-"""
-
 import uuid
 from django.shortcuts import render_to_response
 from django.template import RequestContext
