6711bafa997a8b28df588a9ae02802c15445709f
Gabriel Chen <gcthree@princeton.edu>
2012-04-18 02:36:21 +0000

    dropoff pickup time

--- tigerapps/storage/models.py
+++ tigerapps/storage/models.py
@@ -4,6 +4,12 @@ import datetime
 
 
 
+class DropoffPickupTime(models.Model):
+    '''
+    User's selection from possible dropoff and pickup times of an order
+    '''
+    dropoff_time = models.DateTimeField("Dropoff Time", blank=True, null=True)
+    pickup_time = models.DateTimeField("Pickup Time", blank=True, null=True)
     
 class Status(models.Model):
     '''
@@ -18,14 +24,13 @@ class Status(models.Model):
     '''
     
     user = models.ForeignKey(User, related_name="status")
+    dropoff_pickup_time = models.ForeignKey(DropoffPickupTime, related_name = "status")
     cell_number = models.CharField("Cell Phone Number", max_length=14)
     proxy_name = models.CharField("Proxy Name", max_length=50, blank=True)
     proxy_email = models.CharField("Proxy Email", max_length=50, blank=True)
     n_boxes_paid = models.IntegerField("Number of Boxes Paid For", max_length=2)
     bool_boxes_empty = models.BooleanField("Picked up Empty Boxes", blank=True, default=False)
-    dropoff_time = models.DateTimeField("Dropoff Time", blank=True, null=True)
     n_boxes_dropped = models.IntegerField("Number of Boxes Dropped Off", max_length=2, blank=True, default=0)
-    pickup_time = models.DateTimeField("Pickup Time", blank=True, null=True)
     n_boxes_picked = models.IntegerField("Number of Boxes Picked Up", max_length=2, blank=True, default=0)
     
     def __unicode__(self):
@@ -33,3 +38,4 @@ class Status(models.Model):
     
     class Meta:
         verbose_name_plural = 'Statuses'
+    
