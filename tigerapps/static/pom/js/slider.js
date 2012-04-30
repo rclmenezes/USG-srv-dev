910276d4aabe243454af6bc90b225cb61828a243
Nader Al-Naji <nbal@gmail.com>
2012-04-30 02:15:08 +0000

    added slider business

--- tigerapps/static/pom/js/slider.js
+++ tigerapps/static/pom/js/slider.js
@@ -1,15 +1,26 @@
 //This contains all the code to make the slider work.
 
+function convertToDate(sliderVal) {
+    return sliderVal;
+}
+
 function sliderInit() {
-		$( "#jmap-slider" ).slider();
-		/*$( "#jmap-slider" ).slider({
-			range: true,
-			min: 0,
-			max: 500,
-			values: [ 75, 300 ],
-			slide: function( event, ui ) {
-				$( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-			}
-		});*/
-		alert('hhh')
+    $( "#jmap-slider" ).slider({
+        range: true,
+        min: 0,
+        max: 500,
+        values: [0, 500],
+        slide: function( event, ui ) {
+            $( "#slider-left-value" ).val( ui.values[ 0 ] );
+            sliderLeftDate = convertToDate(ui.values[0]);
+            $( "#slider-right-value" ).val( ui.values[ 1 ] );
+            sliderRightDate = convertToDate(ui.values[1]);
+        }
+    });
+    $( "#slider-left-value" ).val($( "#jmap-slider" ).slider( "values", 0 ));
+    sliderLeftDate = convertToDate($( "#jmap-slider" ).slider( "values", 0 ));
+    $( "#slider-right-value" ).val($( "#jmap-slider" ).slider( "values", 1 ) );	
+    sliderRightDate = convertToDate($( "#jmap-slider" ).slider( "values", 1 ));
+
+alert('hhk')
 } 
