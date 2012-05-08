4514bede2239901971cfd977e866316a756a1504
Lawrence Diao <Lawrence.Diao91@gmail.com>
2012-05-08 05:48:19 +0000

    more efficient filtering for date/building

--- tigerapps/pom/cal_event_query.py
+++ tigerapps/pom/cal_event_query.py
@@ -28,3 +28,8 @@ def date_filtered(leftMonth, leftDay, leftYear, leftHour, rightMonth, rightDay, 
     right = datetime.datetime(year = int(rightYear) -1, month = int(rightMonth), day = int(rightDay), hour = int(rightHour))
     return Event.objects.filter(event_date_time_start__gte=left, event_date_time_end__lte=right)
 
+def filter_res_by_date(res,leftMonth, leftDay, leftYear, leftHour, rightMonth, rightDay, rightYear, rightHour):
+    left = datetime.datetime(year = int(leftYear) -1, month = int(leftMonth), day = int(leftDay), hour = int(leftHour))
+    right = datetime.datetime(year = int(rightYear) -1, month = int(rightMonth), day = int(rightDay), hour = int(rightHour))
+    return res.filter(event_date_time_start__gte=left, event_date_time_end__lte=right)
+
--- tigerapps/pom/views.py
+++ tigerapps/pom/views.py
@@ -71,9 +71,9 @@ def events_for_bldg(request, bldg_code):
     if filter_type == '0':
         #0 = standard event
         try:
-            #events = cal_event_query.bldg_filtered(bldg_code)
-            events = cal_event_query.date_filtered(request.GET['m0'], request.GET['d0'], request.GET['y0'], request.GET['h0'],
-                                                  request.GET['m1'], request.GET['d1'], request.GET['y1'], request.GET['h1']).filter(event_location=bldg_code)
+            events = cal_event_query.filter_res_by_date(cal_event_query.bldg_filtered(bldg_code), 
+                                                        request.GET['m0'], request.GET['d0'], request.GET['y0'], request.GET['h0'],
+                                                        request.GET['m1'], request.GET['d1'], request.GET['y1'], request.GET['h1'])
             html = render_to_string('pom/event_info.html',
                                     {'bldg_name': BLDG_INFO[bldg_code][0],
                                      'events': events})
