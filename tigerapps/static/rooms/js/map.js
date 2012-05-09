8f5bbb00bdf9d30bdf27b7b852f05f11944105f1
John O'Neill <jconeill@dev.tigerapps.org>
2012-05-09 19:34:35 +0000

    rooms: fancybox

--- tigerapps/static/rooms/js/map.js
+++ tigerapps/static/rooms/js/map.js
@@ -132,4 +132,17 @@ function initialize() {
     // Set the map to start draw
     switchdraw({}, 1);
 
+    // attach fancybox to appropriate links
+    $(".various").fancybox({
+		maxWidth	: 800,
+		maxHeight	: 600,
+		fitToView	: false,
+		width		: '70%',
+		height		: '70%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
+
 }
