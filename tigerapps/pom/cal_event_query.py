4556cf879cca88d578280de44f593ff0e72b68a7
Lawrence Diao <Lawrence.Diao91@gmail.com>
2012-05-09 08:45:18 +0000

    events are put in in sorted order, and google cal and ical integration is buggily completed

--- tigerapps/pom/cal_event_query.py
+++ tigerapps/pom/cal_event_query.py
@@ -32,5 +32,5 @@ def filter_res_by_date(res,leftMonth, leftDay, leftYear, leftHour, rightMonth, r
     '''DONT FORGET TO CHANGE THIS. YEAR SHOULD NOT HAVE THE -1 IN IT!!!!!'''
     left = datetime.datetime(year = int(leftYear) -1, month = int(leftMonth), day = int(leftDay), hour = int(leftHour))
     right = datetime.datetime(year = int(rightYear) -1, month = int(rightMonth), day = int(rightDay), hour = int(rightHour))
-    return res.filter(event_date_time_start__gte=left, event_date_time_end__lte=right)
+    return res.filter(event_date_time_start__gte=left, event_date_time_end__lte=right).order_by('event_date_time_start','event_date_time_end')
 
