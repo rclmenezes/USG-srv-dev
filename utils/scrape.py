9bc99fc2124969c24231979e3642cf80d09cbeb6
Ben Grange <bgrange@princeton.edu>
2012-05-09 01:18:58 +0000

    scrape.py

---
+++ utils/scrape.py
@@ -0,0 +1,15 @@
+#general scraping utility using curl, to avoid apache gayness
+
+import subprocess as sub
+
+def log(s):
+    f = open('/srv/tigerapps/slog', 'a')
+    f.write(s + '\n')
+    f.close()
+
+def scrapePage(url):
+    '''Return a unicode string of data at the specified url'''
+    log('scraping ' + url)
+    data = unicode(sub.check_output(["curl", url]), 'utf-8')
+    log('done scraping')
+    return data
