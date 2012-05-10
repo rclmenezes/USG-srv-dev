cd0b3866e690d178f99e51b5aae4cf78ea180eea
Josh Chen <joshchen@princeton.edu>
2012-05-10 01:19:13 +0000

    working

--- tigerapps/pom/urls.py
+++ tigerapps/pom/urls.py
@@ -16,7 +16,7 @@ urlpatterns = patterns('',
     url(r'^events/bldg/(?P<bldg_code>\S+)/?$', 'pom.views.events_for_bldg'),
     url(r'^events/all/?$', 'pom.views.events_for_all_bldgs'),
     url(r'^bldgs/name/?$', 'pom.views.get_bldg_names_json'),
-    url(r'^events/json$', 'pom.views.get_cal_events_json')
+    url(r'^events/json$', 'pom.views.get_cal_events_json'),
 
     url(r'^pmap/?$', direct_to_template,
         {'template': 'pom/pmap.html'}),
