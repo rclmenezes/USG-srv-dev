bb3c70c41f3f8819404e3b9536a9d46ccbd08458
John McSpedon <johnmc1547@gmail.com>
2012-05-10 09:18:18 +0000

    fixed error in locations

--- tigerapps/static/pom/js/map.js
+++ tigerapps/static/pom/js/map.js
@@ -381,12 +381,8 @@ function displayLocationBldgs() {
 }
 /* We also need a way to color just 1 building (the one the user searched for) */
 function displayLocationBldg(bldgId) {
-	alert(bldgId);
 	displayLocationBldgs();
-	alert("here2");
-	setupEventBldg(/*JOSHBOPS this variable is undefined, needs to be domEle */ jmap.loadedBldgs[bldgId]);
-	alert(bldgId);
-	alert(jmap.loadedBldgs[bldgId]);
+	setupEventBldg(jmap.loadedBldgs[bldgId].domEle);
 }
 
 function showMapLoading() {
